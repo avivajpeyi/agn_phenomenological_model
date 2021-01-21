4ebdcf9b430e5acd1ee2c8d24ed8686e70f677ae
avi.vajpeyi <avi.vajpeyi@ligo.org>
2021-01-21 20:56:29 +1100

    add correct path

--- scripts/get_gwtc2_data.py
+++ scripts/get_gwtc2_data.py
@@ -1,14 +1,15 @@
 import os, glob
 from tqdm import tqdm
-import pesummary.io
+import pesummary
+from pesummary.io import read, write
 import pandas as pd
 
-# logging.getLogger("PESummary").setLevel(logging.ERROR)
+
 
 outdir = "../data/downsampled_pe_samples"
 os.makedirs(outdir, exist_ok=True)
 MAX_NUM_SAMPLES = 5000
-files = glob.glob("/home/shanika.galaudage/O3/population/o3a_pe_samples_release/*.h5")
+files = glob.glob("/home/avi.vajpeyi/projects/jupyter_notebooks/GW190408_181802/GW190408_181802.h5")
 for res_file in tqdm(files, total=len(files)):
     event_name = os.path.basename(res_file).split(".h")[0].split("_")[0]
     posterior_filename = f"{outdir}/{event_name}.csv"
@@ -18,20 +19,26 @@ for res_file in tqdm(files, total=len(files)):
         pass
     else:
         try:
+            print(f"Opening {res_file}")
             data = pesummary.io.read(res_file)
+            print(data.summary)
             if len(data.samples) > MAX_NUM_SAMPLES:
                 data.downsample(MAX_NUM_SAMPLES)
-
-            parameters = data.parameters
-            posterior_samples = data.samples
+            
+            print('Found run labels:')
+            print(data.labels)
+            samples_dict = data.samples_dict
+            posterior_samples = samples_dict['PublicationSamples']
+            parameters = sorted(list(posterior_samples.keys()))
+            print(parameters)
             prior_samples = pd.DataFrame(data.priors['samples']).to_numpy()
             kwargs = dict(parameters=parameters, file_format="csv")
 
             if not os.path.isfile(posterior_filename):
-                pesummary.io.write(samples=posterior_samples,
+                write(samples=posterior_samples,
                                    filename=posterior_filename, **kwargs)
             if not os.path.isfile(prior_filename):
-                pesummary.io.write(samples=prior_samples, filename=prior_filename,
+                write(samples=prior_samples, filename=prior_filename,
                                    **kwargs)
         except Exception as e:
             print(f"ERROR {event_name}: {e}")
