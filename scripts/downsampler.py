79aba5f83c6e50759f5e341941e07a3593e8817d
avi.vajpeyi <avi.vajpeyi@ligo.org>
2021-01-22 12:55:16 +1100

    uopdate ownsampler

--- scripts/downsampler.py
+++ scripts/downsampler.py
@@ -4,24 +4,29 @@ from pprint import pprint
 
 MAX_SAMPLES = 5000
 
-fnames = []
-lens = []
-files = glob.glob("../data/gwtc_samples/*.csv")
-for f in tqdm.tqdm(files):
-    try:
-        lens.append(len(pd.read_csv(f, sep=" ")))
-        fnames.append(f)
-    except Exception as e:
-        print(f"ERROR {f}: {e}")
+def main():
 
-samples_count = pd.DataFrame(dict(fnames=fnames,lens=lens))
-samples_count = samples_count[samples_count['lens']>MAX_SAMPLES]
-samples_count = samples_count.reset_index(drop=True)
-print(samples_count)
+    fnames = []
+    lens = []
+    files = glob.glob("../data/gwtc_samples/*.csv")
+    for f in tqdm.tqdm(files):
+        try:
+            lens.append(len(pd.read_csv(f, sep=" ")))
+            fnames.append(f)
+        except Exception as e:
+            print(f"ERROR {f}: {e}")
+
+    samples_count = pd.DataFrame(dict(fnames=fnames,lens=lens))
+    samples_count = samples_count[samples_count['lens']>MAX_SAMPLES]
+    samples_count = samples_count.reset_index(drop=True)
+    print(samples_count)
 
 
-for i in range(len(samples_count)):
-    fname = samples_count.iloc[i]['fnames']
-    df = pd.read_csv(fname, sep=" ").sample(MAX_SAMPLES)
-    df.to_csv(fname, sep=' ', index=False)
-    print(f"{fname} len {samples_count.iloc[i]['lens']}-->{MAX_SAMPLES}" )
+    for i in range(len(samples_count)):
+        fname = samples_count.iloc[i]['fnames']
+        df = pd.read_csv(fname, sep=" ").sample(MAX_SAMPLES)
+        df.to_csv(fname, sep=' ', index=False)
+        print(f"{fname} len {samples_count.iloc[i]['lens']}-->{MAX_SAMPLES}" )
+        
+if __name__=="__main__":
+    main()
