79653803400c37bb911f29c812b1aa00a253e327
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-01-22 12:50:59 +1100

    refactoring

--- scripts/test.py
+++
@@ -1,5 +0,0 @@
-import pesummary
-from pesummary.io import read
-
-data = read("/home/zoheyr.doctor/public_html/O3/O3aCatalog/data_release/all_posterior_samples/GW190517_055101.h5")
-print(data.summary)
