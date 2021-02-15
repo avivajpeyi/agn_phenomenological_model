6650d5128b450db5a07466d7f48be2b1daf13cf7
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-02-15 18:36:16 +1100

    add new inis

--- population_inference/runner.sh
+++ population_inference/runner.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-gwpopulation_pipe agn_population.ini --submit
-gwpopulation_pipe mixed_population.ini --submit
+gwpopulation_pipe agn_population.ini
+gwpopulation_pipe mixed_population.ini
+gwpopulation_pipe lsc_o3a.ini
+gwpopulation_pipe fixed_xi_0.ini
+gwpopulation_pipe fixed_xi_1.ini
