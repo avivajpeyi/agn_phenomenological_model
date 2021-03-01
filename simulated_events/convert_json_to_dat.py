25d975d0e8bf58e6f0d31b74bf398febd336e4b1
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-03-01 13:37:26 +1100

    update dat saver

--- simulated_events/convert_json_to_dat.py
+++ simulated_events/convert_json_to_dat.py
@@ -8,6 +8,16 @@ from tqdm import tqdm
 DAT_OUTDIR = "simulated_event_samples"
 JSON_REGEX = "outdir_agn_injections/result/*result.json"
 
+PARAM = [
+    'a_1',
+    'a_2',
+    'cos_tilt_1',
+    'cos_theta_12',
+    'mass_ratio',
+    'mass_1',
+    'redshift',
+]
+
 
 def main():
     if not os.path.isdir(DAT_OUTDIR):
@@ -20,10 +30,12 @@ def main():
 
 
 def save_cbc_dat(posterior: pd.DataFrame, old_fn: str):
+    posterior['mass_1'] = posterior["mass_1_source"]
+    posterior["mass_2"] = posterior["mass_2_source"]
+    posterior = posterior[PARAM]
     new_fn = os.path.basename(old_fn).replace(".json", ".dat")
     new_fn = os.path.join(DAT_OUTDIR, new_fn)
     posterior.to_csv(new_fn, sep=" ", index=False)
 
-
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
