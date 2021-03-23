844b0ad07a3cce81c476c767aab5fdc61579b34d
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-03-23 15:11:20 +1100

    pdf maker

--- simulated_events/pe_corner_plotter/combine_corners_into_pdf.py
+++ simulated_events/pe_corner_plotter/combine_corners_into_pdf.py
@@ -9,7 +9,7 @@ def combine_images_to_pdf():
     sdir = "/home/avi.vajpeyi/public_html/agn_pop/simulated_events/pe_set_3/"
     w, h = 0, 0
 
-    for i in range(0, 1):
+    for i in range(0, 100):
         fname = sdir + f"inj{i}_data0_0_analysis_H1L1_dynesty_result.png"
         if os.path.exists(fname):
             if i == 0:
