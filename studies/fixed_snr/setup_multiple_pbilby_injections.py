059b39632727f4af9baa5939e809071ca57912a7
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-06-16 14:36:46 +1000

    update num injections

--- studies/fixed_snr/setup_multiple_pbilby_injections.py
+++ studies/fixed_snr/setup_multiple_pbilby_injections.py
@@ -4,13 +4,11 @@ Module to create an injection file + pbilby inis for the injections.
 import logging
 import os
 import shutil
-
+import pandas as pd
 from bilby_pipe.create_injections import create_injection_file
 
 logging.getLogger().setLevel(logging.INFO)
 
-
-N_INJECTION = 100
 LABEL = "snr60"  # the main name of the injections
 INJECTION_FILE = f"./datafiles/{LABEL}_injections.dat"
 PRIOR_FILE = "./datafiles/bbh.prior"
@@ -31,12 +29,12 @@ def create_ini(injection_idx: int):
         f.write(txt)
 
 
-def create_data_generation_slurm_submission_file():
+def create_data_generation_slurm_submission_file(num_inj):
     os.makedirs("generation_log", exist_ok=True)
     with open("pbilby_generation_job_template.ini", "r") as f:
         txt = f.read()
         txt = txt.replace("{{{GENERATION_LOG_DIR}}}", "generation_log")
-        txt = txt.replace("{{{NUM_INJECTIONS}}}", str(N_INJECTION))
+        txt = txt.replace("{{{NUM_INJECTIONS}}}", str(num_inj))
         txt = txt.replace("{{{LABEL}}}", LABEL)
         txt = txt.replace(
             "{{{GENERATION_EXE}}}", shutil.which("parallel_bilby_generation")
@@ -45,10 +43,10 @@ def create_data_generation_slurm_submission_file():
         f.write(txt)
 
 
-def create_analysis_bash_runner():
+def create_analysis_bash_runner(num_inj):
     file_contents = "#! /bin/sh\n"
-    for i in range(N_INJECTION):
-        label = f"{LABEL}_{i}"
+    for i in range(num_inj):
+        label = f"{LABEL}_{i:02}"
         analysis_file = f"out_{label}/submit/bash_{label}.sh"
         file_contents += f"bash {analysis_file}\n"
     with open("start_data_analysis.sh", "w") as f:
@@ -57,13 +55,14 @@ def create_analysis_bash_runner():
 
 def main():
     logging.info("Generating parallel bilby ini files + submission scripts")
-    for i in range(N_INJECTION):
+    num_inj = len(pd.read_csv(INJECTION_FILE))
+    for i in range(num_inj):
         create_ini(injection_idx=i)
 
-    create_data_generation_slurm_submission_file()
+    create_data_generation_slurm_submission_file(num_inj)
     logging.info("Start generation jobs with:\n$ sbatch slurm_data_generation.sh")
 
-    create_analysis_bash_runner()
+    create_analysis_bash_runner(num_inj)
     logging.info(
         "After generation, begin analysis with:\n$ bash start_data_analysis.sh"
     )
