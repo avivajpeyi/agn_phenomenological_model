95702498e94ab80c989ba81eb7dcfbe4a355474a
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-06-17 10:42:20 +1000

    add priors

--- studies/fixed_snr/plot_corners.py
+++ studies/fixed_snr/plot_corners.py
@@ -13,7 +13,7 @@ def main():
     plot_dir = "plot_out"
     print(f"Plotting corners for {len(files)} files.")
     os.makedirs(plot_dir, exist_ok=True)
-    prior=bilby.prior.PriorDict(from_file=PRIORS)
+    prior=bilby.prior.PriorDict(filename=PRIORS)
     for f in tqdm(files):
         r = bilby.gw.result.CBCResult.from_json(f)
         fname = os.path.basename(f).replace(".json", ".png")
