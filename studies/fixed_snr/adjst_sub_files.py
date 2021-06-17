cc0044a7a8c163fd3e26f4f0e3fa3c6607c0cbb6
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-06-17 15:30:20 +1000

    hack to increase mem

--- studies/fixed_snr/adjst_sub_files.py
+++ studies/fixed_snr/adjst_sub_files.py
@@ -6,5 +6,5 @@ sub_files = glob.glob(sub_file_regex)
 
 for f in sub_files:
     content = open(f, 'r').read()
-    content = content.replace("mem-per-cpu=2000", "mem-per-cpu=4000")
+    content = content.replace("mem-per-cpu=4000", "mem-per-cpu=8000")
     open(f, 'w').write(content)
