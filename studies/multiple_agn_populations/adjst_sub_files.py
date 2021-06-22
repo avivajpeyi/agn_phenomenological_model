876804882827e40194111dc9d5baad7d852503a3
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-06-22 11:51:35 +1000

    adjust submission

--- studies/multiple_agn_populations/adjst_sub_files.py
+++ studies/multiple_agn_populations/adjst_sub_files.py
@@ -7,8 +7,7 @@ sub_files = glob.glob(sub_file_regex)
 
 for f in tqdm(sub_files):
     content = open(f, 'r').read()
-    # SBATCH --ntasks-per-node=16
-    content = content.replace("ntasks-per-node=14", "ntasks=16")
-    content = content.replace("MPI_PER_NODE=14", "MPI_PER_NODE=16")
-    content = content.replace("mem-per-cpu=8000", "mem-per-cpu=2000")
+    content = content.replace("ntasks-per-node=16", "ntasks-per-node=14")
+    content = content.replace("MPI_PER_NODE=16", "MPI_PER_NODE=14")
+    content = content.replace("mem-per-cpu=8000", "mem-per-cpu=4000")
     open(f, 'w').write(content)
