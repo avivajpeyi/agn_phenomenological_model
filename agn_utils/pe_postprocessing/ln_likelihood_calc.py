d04247573a6cb9be255a987690f7e9ed5e077736
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-08-11 00:04:37 +1000

    add sampler setting tester

--- agn_utils/pe_postprocessing/ln_likelihood_calc.py
+++ agn_utils/pe_postprocessing/ln_likelihood_calc.py
@@ -1,18 +1,16 @@
-import bilby
-import numpy as np
 import pickle
 
-def setup_likelihood(res_file, data_dump_file, reference_frequency, waveform, duration=4,sampling_frequency=2048):
+import bilby
 
-    res  = bilby.gw.result.CBCResult.from_json(filename=res_file)
 
+def setup_likelihood(res_file, data_dump_file):
+    res = bilby.gw.result.CBCResult.from_json(filename=res_file)
 
     with open(data_dump_file, "rb") as file:
         data_dump = pickle.load(file)
 
     ifo_list = data_dump["ifo_list"]
     waveform_generator = data_dump["waveform_generator"]
-    # waveform_generator.start_time = ifo_list[0].time_array[0]
     injection_parameters = data_dump.get("injection_parameters", None)
 
     likelihood = bilby.gw.GravitationalWaveTransient(
@@ -23,17 +21,18 @@ def setup_likelihood(res_file, data_dump_file, reference_frequency, waveform, du
     # print(likelihood.parameters)
     true_logL = likelihood.log_likelihood()
     max_likelihood_posterior = max(res.posterior.log_likelihood)
+    max_lnl_param = res.posterior.to_dict('records')[-1]
+    likelihood.parameters.update(max_lnl_param)
+    max_likelihood_posterior = likelihood.log_likelihood()
 
-
-    print(f"MaxPosterior LnL: {max_likelihood_posterior}\n"
-          f"LnL(true val): {true_logL}")
-
+    print(
+        f"max(posterior LnL): {max(res.posterior.log_likelihood) + res.log_noise_evidence}\n"
+        # f"LnL(posterior max LnL param): {max_likelihood_posterior}\n"
+        f"LnL(true val): {true_logL}")
+    pass
 
 
 setup_likelihood(
     res_file="../../studies/snr50_100_fixedphase/outdir_pop_a_highsnr/out_pop_a_highsnr_01/result/pop_a_highsnr_01_0_result.json",
     data_dump_file="../../studies/snr50_100_fixedphase/outdir_pop_a_highsnr/out_pop_a_highsnr_01/data/pop_a_highsnr_01_data_dump.pickle",
-    reference_frequency=20,
-    waveform="IMRPheonomPXHM",
-    duration=4,sampling_frequency=2048
 )
---
+++ studies/determining_sampler_settings/README.md
@@ -0,0 +1,34 @@
+# Injection Param
+
+"injection_parameters": {
+  "mass_1": 56.87440532835858,
+  "mass_2": 56.87440532835858,
+  "tilt_1": 0.8097166604301091,
+  "tilt_2": 1.2305090549019628,
+  "phi_12": 4.6443532713696865,
+  "a_1": 0.6000000000000001,
+  "a_2": 0.6000000000000001,
+  "phi_jl": 2.376897181890326,
+  "theta_jn": 1.2269631368327132,
+  "ra": 1.0455107634041807,
+  "dec": -0.533007022881434,
+  "luminosity_distance": 334.87972017754083,
+  "geocent_time": 0.0,
+  "psi": 0.341452015705022,
+  "phase": 0.1,
+  "reference_frequency": 20.0,
+  "h1_snr": 44.330299349188,
+  "l1_snr": 36.298021281040256,
+  "network_snr": 57.295041576976566,
+  "chirp_mass": 49.512045595734605,
+  "duration": 4.0,
+  "cos_tilt_2": 0.3337579052938932,
+  "cos_tilt_1": 0.6897036244885455,
+  "mass_ratio": 1.0
+}
+
+# Sampler settings 
+
+* run_a: nact 5 nlive 1000 (wide priors)
+* run_b: nact 10 nlive 1000 
+* run_c: nact 20 nlive 1000
