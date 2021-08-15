a5293137dd1e2216992aaecca77ec3b5d4f5054b
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-08-16 09:42:44 +1000

    plotter

--- agn_utils/plotting/cdf_plotter/sigma_cdf_difference_check.py
+++ agn_utils/plotting/cdf_plotter/sigma_cdf_difference_check.py
@@ -62,69 +62,74 @@ def plotter(pop_a_regex, pop_b_regex):
 
     pop_a_pkl = 'pop_a.pkl'
     pop_b_pkl = 'pop_b.pkl'
-    dat_a = get_bilby_results(pop_a_regex, pop_a_pkl, ["cos_tilt_1", "cos_theta_12"])
-    simple_violin_plotter(dat_a, "pop_a_pe.png")
-    pe_cdf(
-        pops_dat_dicts=dict(
-            pop_a=dat_a,
-        ),
-        true_pop_params=dict(
-            popA=[0.5, 3]
-        ),
-        title="Population A", fname="pop_a_cdf.png",
-        colors1=[TruePopA, DataA],
-        colors2=[TruePopA, DataA]
-    )
 
-    dat_a["posteriors"] = rejection_sample_population(dat_a["posteriors"], true_population_param=dict(sigma_1=0.5, sigma_12=3))
-    simple_violin_plotter(dat_a, "pop_a_pe_reweighted.png")
-    pe_cdf(
-        pops_dat_dicts=dict(
-            pop_a=dat_a,
-        ),
-        true_pop_params=dict(
-            popA=[0.5, 3]
-        ),
-        title="Population A Reweighted", fname="pop_a_cdf_reweighted.png",
-        colors1=[TruePopA, DataA],
-        colors2=[TruePopA, DataA]
-    )
+    if len(pop_a_regex) > 0:
+        dat_a = get_bilby_results(pop_a_regex, pop_a_pkl, ["cos_tilt_1", "cos_theta_12"])
+        simple_violin_plotter(dat_a, "pop_a_pe.png")
+        pe_cdf(
+            pops_dat_dicts=dict(
+                pop_a=dat_a,
+            ),
+            true_pop_params=dict(
+                popA=[0.5, 3]
+            ),
+            title="Population A", fname="pop_a_cdf.png",
+            colors1=[TruePopA, DataA],
+            colors2=[TruePopA, DataA]
+        )
 
-    dat_b = get_bilby_results(pop_b_regex, pop_b_pkl, ["cos_tilt_1", "cos_theta_12"])
-    simple_violin_plotter(dat_a, "pop_a_pe_reweighted.png")
-    pe_cdf(
-        pops_dat_dicts=dict(
-            pop_B=dat_b,
-        ),
-        true_pop_params=dict(
-            popB=[1, 0.25]
-        ),
-        title="Population B", fname="pop_b_cdf.png",
-        colors1=[TruePopB, DataB],
-        colors2=[TruePopB, DataB]
-    )
-    dat_b["posteriors"] = rejection_sample_population(dat_b["posteriors"], true_population_param=dict(sigma_1=1, sigma_12=0.25))
-    pe_cdf(
-        pops_dat_dicts=dict(
-            pop_b=dat_b,
-        ),
-        true_pop_params=dict(
-            popB=[1, 0.25]
-        ),
-        title="Population B", fname="pop_b_cdf_reweighted.png",
-        colors1=[TruePopB, DataB],
-        colors2=[TruePopB, DataB]
-    )
-    pe_cdf(
-        pops_dat_dicts=dict(
-            pop_a=dat_a,
-            pop_b=dat_b
-        ),
-        true_pop_params=dict(),
-        title="Population Comparision (reweighted)", fname="pop_compare_cdf_reweighted.png",
-        colors1=[TruePopA, TruePopB, DataA,  DataB],
-        colors2=[TruePopA, TruePopB, DataA, DataB]
-    )
+        dat_a["posteriors"] = rejection_sample_population(dat_a["posteriors"], true_population_param=dict(sigma_1=0.5, sigma_12=3))
+        simple_violin_plotter(dat_a, "pop_a_pe_reweighted.png")
+        pe_cdf(
+            pops_dat_dicts=dict(
+                pop_a=dat_a,
+            ),
+            true_pop_params=dict(
+                popA=[0.5, 3]
+            ),
+            title="Population A Reweighted", fname="pop_a_cdf_reweighted.png",
+            colors1=[TruePopA, DataA],
+            colors2=[TruePopA, DataA]
+        )
+
+    if len(pop_b_regex) > 0:
+        dat_b = get_bilby_results(pop_b_regex, pop_b_pkl, ["cos_tilt_1", "cos_theta_12"])
+        simple_violin_plotter(dat_a, "pop_a_pe_reweighted.png")
+        pe_cdf(
+            pops_dat_dicts=dict(
+                pop_B=dat_b,
+            ),
+            true_pop_params=dict(
+                popB=[1, 0.25]
+            ),
+            title="Population B", fname="pop_b_cdf.png",
+            colors1=[TruePopB, DataB],
+            colors2=[TruePopB, DataB]
+        )
+        dat_b["posteriors"] = rejection_sample_population(dat_b["posteriors"], true_population_param=dict(sigma_1=1, sigma_12=0.25))
+        pe_cdf(
+            pops_dat_dicts=dict(
+                pop_b=dat_b,
+            ),
+            true_pop_params=dict(
+                popB=[1, 0.25]
+            ),
+            title="Population B", fname="pop_b_cdf_reweighted.png",
+            colors1=[TruePopB, DataB],
+            colors2=[TruePopB, DataB]
+        )
+
+    if len(pop_a_regex) > 0 and  len(pop_b_regex) > 0:
+        pe_cdf(
+            pops_dat_dicts=dict(
+                pop_a=dat_a,
+                pop_b=dat_b
+            ),
+            true_pop_params=dict(),
+            title="Population Comparision (reweighted)", fname="pop_compare_cdf_reweighted.png",
+            colors1=[TruePopA, TruePopB, DataA,  DataB],
+            colors2=[TruePopA, TruePopB, DataA, DataB]
+        )
 
 
 
--- studies/comoving_dist_injections/README.md
+++ studies/comoving_dist_injections/README.md
@@ -1,6 +1,7 @@
 draw_population_samples --outdir pop_files --prior-file comoving_bbh.prior
 
-pbilby_pe_jobgen --pop-files pop_files/pop_a_highsnr.dat --prior-file comoving_bbh.prior --psd-file ../data/aLIGO_late_psd.txt
+pbilby_pe_jobgen --pop-files pop_files/pop_a_validsnr.dat --prior-file comoving_bbh.prior --psd-file ../data/aLIGO_late_psd.txt --cluster sstar 
+pbilby_pe_jobgen --pop-files pop_files/pop_b_validsnr.dat --prior-file comoving_bbh.prior --psd-file ../data/aLIGO_late_psd.txt --cluster gstar
 
 cdf_check --pop-a-regex "outdir/pop_a/*.json" --pop-b-regex "outdir/pop_b/*.json" 
 
