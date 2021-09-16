f7fe1e9f3c094b84e3e3e7ecc0c27e5c067e8fbf
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-09-16 15:37:54 +1000

    add python runner scripts'

--- studies/comoving_dist_injections/reweighter_and_tilt_at_infty/save_h5_posteriors_to_pkl.py
+++ studies/comoving_dist_injections/reweighter_and_tilt_at_infty/save_h5_posteriors_to_pkl.py
@@ -8,16 +8,19 @@ REGEX = dict(
     pop_b="/fred/oz980/avajpeyi/projects/agn_phenomenological_model/studies/comoving_dist_injections/outdir_pop_b_validsnr_clean/out_*/result/*_reweighted.h5",
 )
 
-def store_posteriors_in_pkl(regex):
+def store_posteriors_in_pkl(regex, label):
     files = glob.glob(regex)
     data, fnames = [], []
-    for f in tqdm(files):
+    for f in tqdm(files, desc="Extracting posteriors"):
         df = pd.read_hdf(f, 'samples')
         fnames.append(f)
         data.append(df.to_dict('list'))
     data = ld_to_dl(data)
-    print(data)
-    with open(fname, 'wb') as f:
+
+    archive_fname = f"{label}.pkl"
+    print(f"Saving {archive_fname}")
+
+    with open(archive_fname, 'wb') as f:
         pickle.dump(dict(posteriors=data, fnames=fnames), f)
 
 def main():
